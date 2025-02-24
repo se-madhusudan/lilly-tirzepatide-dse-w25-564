703dbda985dcaf7d856074ea542dc9f7a5c70255
Dharminder7 <madhusudan.kumar@tmotions.com>
2025-02-24 13:27:59 +0530

    barriers responsiveness done

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -123,7 +123,7 @@ export default {
       managementData: {
         infoData: {
           source: require('@/Assets/Img/icons/dse-icon-weight-mgmt-lg.svg'),
-          title: 'Importance of early glycemic control and weight<br /> management in T2D',
+          title: 'Importance of early glycemic control and weight<br class="breakHide" /> management in T2D',
           subTitle: 'In patients with T2D, a treatment approach that prioritizes weight management in support of glycemic control may result in more comprehensive T2D management.<sup>2,3,25</sup>',
           des: [
             'In the Look AHEAD (Action for Health in Diabetes) study, which evaluated intensive lifestyle intervention (ILI) compared to conventional diabetes support and education in adults with T2D and overweight or obesity, participants in the ILI group were more likely to lose weight and to achieve ancillary benefits of decreased medication use.<sup>16,18,28,29</sup>',
